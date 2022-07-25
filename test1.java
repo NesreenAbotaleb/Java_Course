5fd846be84b6f00a8e203f64e102cb47ff9eff73
unknown <mohamedaboelhawey@gmail.com>
2022-07-25 20:37:12 +0200

    Java Course

---
+++ test1.java
@@ -0,0 +1,11 @@
+package EX01_01;
+
+public class test1 {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to java ");
+        System.out.println("Welcom to Computer Science ");
+        System.out.println("Programming is fun");
+    }
+
+}
