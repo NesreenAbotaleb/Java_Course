536d155950869cf04b3cf75077ac400028a353cc
unknown <mohamedaboelhawey@gmail.com>
2022-07-25 20:49:34 +0200

    EX1.2

---
+++ test2.java
@@ -0,0 +1,15 @@
+
+package EX01_02;
+
+
+public class test2 {
+   public static void main(String[]args){
+   int x = 0;
+   while(x<5){
+       System.out.println("Welcome to Java");
+       x++;
+   }
+   }
+
+}
+
