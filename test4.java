5e45e3f7fe197694e4fd2d46688979484dfe4dd3
unknown <mohamedaboelhawey@gmail.com>
2022-07-29 18:54:45 +0200

    EX01_04

---
+++ test4.java
@@ -0,0 +1,17 @@
+
+package EX01_04;
+
+
+public class test4 {
+    public static void main (String[]args){
+        System.out.println("a  a^2  a^3");
+        
+        for( int i=1; i<=4;i++){
+        int a=i ;
+        int a2=i*i;
+        int a3= i*i*i;
+            System.out.println(a+"  "+a2+"    "+a3);
+        
+        }
+    }
+}
