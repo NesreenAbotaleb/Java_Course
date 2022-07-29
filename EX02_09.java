bcdfb200e0de9a5202aa4a3a0ae7509f580e85f3
unknown <mohamedaboelhawey@gmail.com>
2022-07-29 20:30:14 +0200

    EX 2.9

---
+++ EX02_09.java
@@ -0,0 +1,19 @@
+package EX02_09;
+import java.util.*;
+public class EX02_09 {
+    public static void main (String[]args){
+    
+        System.out.print("Enter v0, v1, and t : ");
+        
+        Scanner input = new Scanner (System.in);
+    double v0 = input.nextDouble();
+    double v1 = input.nextDouble();
+    double t = input.nextDouble();
+    
+    
+    double a = (v1-v0)/t;
+    
+        System.out.println("The average acceleration is "+a);
+        
+    }
+}
